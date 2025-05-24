a2cce1cfc650ce2ba583ac5c45929b1f8d959d7e
Satvik Jain <satvik.jain.kht@gmail.com>
2025-05-24 08:59:28 +0530

    Time: 7 ms (16.72%) | Memory: 20.9 MB (28.03%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,39 +1,42 @@
 class Solution {
-private:
-    void bfs(vector<int>& vis, int i, vector<vector<int>>& adj)
-    {
-        queue <int> q;
-        q.push(i);
-        vis[i] = 1;
-        while(!q.empty()){
-            int temp = q.front();
-            q.pop();
-            for (auto it : adj[temp]){
-                if (!vis[it]){
-                    q.push(it);
-                    vis[it] = 1;
-                }
-            }
-        }
-    }
 public:
-    int findCircleNum(vector<vector<int>>& am) {
-        int n = am.size();
+    bool bfs(vector<vector<int>>& adj, vector<int>& vis, int i){
+        queue<int> q;
+        q.push(i);
+        
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            vis[front]++;
+            if (adj[front].size()){
+                for(auto it: adj[front]){
+                    if (vis[it] == 0){
+                        vis[it]++;
+                        q.push(it);
+                    }
+                }
+        }
+        }
+        return true;
+    }
+    int findCircleNum(vector<vector<int>>& a) {
+        int n = a.size();
         vector<vector<int>> adj(n);
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
-                if (i!=j && am[i][j] == 1){
+                if (a[i][j] == 1 && i != j)
                     adj[i].push_back(j);
-                }
             }
         }
+
         int ans = 0;
-        vector<int> vis(n,0);
-        for (int i = 0; i < n; i++){
-            if (!vis[i]){
-                bfs(vis, i, adj);
-                ans++;
-            }
+
+        vector<int> vis(n, 0);
+        
+        
+        for (int i=0; i<n; i++){
+
+            if ((vis[i]==0) && bfs(adj, vis, i)) ans++;
         }
         return ans;
     }
