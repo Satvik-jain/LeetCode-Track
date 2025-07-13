6cb471b7934dd93e0eda432dfce495d7e7623d32
Satvik Jain <satvik.jain.kht@gmail.com>
2025-07-13 21:42:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (61.74%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        for (int i = 0; i < s.length(); i++){
+            s.push_back(s[0]);
+            s.erase(0, 1);
+            if (s==goal) return true;
+        }
+        return false;
+    }
+};
