3eedc0f481434a656c6a1fd8d361373368c12507
Satvik Jain <satvik.jain.kht@gmail.com>
2025-07-14 14:26:27 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (76.87%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        // vector<vector<int>> dp(n, vector<int>(n, -1));
+        vector<int> dp(n , -1);
         for (int j = 0; j < n; j++) {
-            dp[n-1][j] = matrix[n-1][j];
+            dp[j] = matrix[n-1][j];
         }
         for (int depth = n-2; depth >= 0; depth--){
+            vector<int> temp(n, -1);
             for (int column = 0; column < n; column++){
                 int i = depth, j = column;
-                int down = dp[i + 1][j];
-                int left = (j > 0) ? dp[i + 1][j - 1] : INT_MAX;
-                int right = (j < n - 1) ? dp[i + 1][j + 1] : INT_MAX;
-                dp[depth][column] = matrix[i][j] + min(min(
+                int down = dp[j];
+                int left = (j > 0) ? dp[j - 1] : INT_MAX;
+                int right = (j < n - 1) ? dp[j + 1] : INT_MAX;
+                temp[j] = matrix[i][j] + min(min(
                                         left,
                                         down), 
                                     right
                                     );
             }
+            dp = temp;
         }
-        return *min_element(dp[0].begin(), dp[0].end());
+        return *min_element(dp.begin(), dp.end());
     }
 };
