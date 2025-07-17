f985ad7439a212bc7af5160e456b4d536d2adc22
Satvik Jain <satvik.jain.kht@gmail.com>
2025-07-17 19:00:31 +0530

    Time: 4 ms (79.24%) | Memory: 15.6 MB (44.76%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,25 +1,22 @@
 class Solution {
-public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if (sum<target || (sum+target)%2!=0) return 0;
-        if (sum + target < 0) return 0;
-        sum = (sum+target)/2;
-        vector<vector<int>> t(n+1, vector<int>(sum+1, 0));
-        for(int i = 0; i < n + 1; i++){
-            t[i][0] = 1;
+  public:
+    int findTargetSumWays(vector<int>& arr, int d) {
+        // Code here
+        int n = arr.size();
+        int sum = accumulate(arr.begin(), arr.end(), 0);
+        int W = (sum+d)/2;
+        if (d > sum || -1*sum > d) return 0;
+        if ((sum+d)%2 != 0) return 0;
+        vector<vector<int>> dp(n+1, vector<int> (W+1, 0));
+        for (int i = 0; i < n+1; i++){
+            dp[i][0] = 1;
         }
-        for(int i = 1; i < n + 1; i++){
-            for(int j = 0; j < sum + 1; j++){
-                if (j >= nums[i-1]){
-                    t[i][j] = t[i-1][j] + t[i-1][j-nums[i-1]];
-                }
-                else{
-                    t[i][j] = t[i-1][j];
-                }
+        for (int i = 1; i < n+1; i++){
+            for (int j = 0; j < W+1; j++){
+                if (j >= arr[i-1]) dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]];
+                else dp[i][j] = dp[i-1][j];
             }
         }
-        return t[n][sum];
+        return dp[n][W];
     }
 };
