a0d937cd92283a8abca8bf5a812f1da8881faeb0
Satvik Jain <satvik.jain.kht@gmail.com>
2025-07-19 11:58:09 +0530

    Time: 27 ms (37.56%) | Memory: 26.8 MB (43.74%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,50 +1,43 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string a, string b) {
-        int n = a.size();
-        int m = b.size();
-        if (n==0 || m==0) return "";
-        vector<vector<int>> t(n+1, vector<int>(m+1, 0));
-        for (int i = 1; i < n+1; i++){
-            for (int j = 1; j < m+1; j++){
-                if (a[i-1] == b[j-1]){
-                    t[i][j] = t[i-1][j-1] + 1;
-                }
-                else{
-                    t[i][j] = max(t[i-1][j], t[i][j-1]);
-                }
+    string shortestCommonSupersequence(string str1, string str2) {
+        string s1 = str1, s2 = str2;
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= m; j++){ 
+                if (s1[i-1] == s2[j-1]) dp[i][j] = 1+dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
         }
-        string ans;
-        int i = n, j = m;
-        while(i>0 || j>0){
-            if (j==0){
-                ans.push_back(a[i-1]);
-                i--;
-                continue;
-            }
-            if (i==0){
-                ans.push_back(b[j-1]);
-                j--;
-                continue;
-            }
-            if (a[i-1] == b[j-1]){
-                ans.push_back(a[i-1]);
-                i--;
-                j--;
+        int i = n;
+        int j = m;
+        string s;
+        while(i>0 && j>0){
+            if (s1[i-1] == s2[j-1]){
+                s.push_back(s1[i-1]);
+                i--; j--;
             }
             else{
-                if (t[i-1][j] > t[i][j-1]){
-                    ans.push_back(a[i-1]);
+                if (dp[i-1][j] > dp[i][j-1]){
+                    s.push_back(s1[i-1]);
                     i--;
                 }
-                else{
-                    ans.push_back(b[j-1]);
+                else if (dp[i-1][j] <= dp[i][j-1]){
+                    s.push_back(s2[j-1]);
                     j--;
                 }
             }
         }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        while(i > 0){
+            s.push_back(s1[i-1]);
+            i--;
+        }
+        while(j > 0){
+            s.push_back(s2[j-1]);
+            j--;
+        }
+        reverse(s.begin(), s.end());
+        return s;
     }
 };
