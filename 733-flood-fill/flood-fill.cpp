8bbb086303fec0d6ff2c9a430806ae154c65979c
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-03 01:24:03 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (16.92%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,38 +1,30 @@
 class Solution {
-private:
-    void bfs(vector<vector<int>>& vis, int color, int sr, int sc, vector<vector<int>>& image){
-        queue <pair<int, int>> q;
-        q.push({sr,sc});
-        int oldcolor = image[sr][sc];
-        image[sr][sc] = color;
-        vis[sr][sc] = 1;
-        while(!q.empty()){
-            int sr = q.front().first;
-            int sc = q.front().second;
-            q.pop();
-            for (auto i : {-1,0,1}){
-                for (auto j: {-1,0,1}){
-                    int newr = sr + i;
-                    int newc = sc + j;
-                    if (abs(i) + abs(j) == 2) continue;
-                    if (newr >= 0 && newc >= 0 && newr < image.size() && 
-                        newc < image[0].size()){
-                        if (image[newr][newc] == oldcolor && vis[newr][newc] == 0){
-                            vis[newr][newc] = 1;
-                            image[newr][newc] = color;
-                            q.push({newr, newc});
-                        }
-                    }
-                }
-            }
-        }
-    }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size();
         int m = image[0].size();
-        vector<vector<int>> vis(n, vector<int> (m, 0));
-        bfs(vis, color, sr, sc, image);
+        int travel = image[sr][sc];
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        int dr[] = {-1, 0, 1, 0};
+        int dc[] = {0, 1, 0, -1};
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            image[r][c] = color;
+            vis[r][c] = 1;
+            q.pop();
+            for(int i = 0; i < 4; i++){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if (nr<0 || nc<0 || nr>=n || nc >= m) continue;
+                if (vis[nr][nc] == 1) continue;
+                if (image[nr][nc] == travel){
+                    q.push({nr, nc});
+                }
+            }
+        }
         return image;
     }
 };
