1ca93883e56a2867f67d05f00e52670a73a7a4a8
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-02 21:17:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (79.61%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,23 +2,27 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         int i = 0, j = s.length() - 1;
-        while(j > i){
-            char a = tolower(s[i]);
-            char b = tolower(s[j]);
-            int an = a - 'a';
-            int bn = b - 'a';
-            cout << "a : " << a << ", b : " << b << ", an : " << an << ", bn : " << bn << endl;
-            if ((an < 0 || an > 25) && (an < -49 || an > -40)){
+        while (j > i) {
+            // Skip non-alphanumeric characters
+            if (!isalnum(s[i])) {
                 i++;
                 continue;
             }
-            if ((bn < 0 || bn > 25) && (bn < -49 || bn > -40)){
+            if (!isalnum(s[j])) {
                 j--;
                 continue;
             }
-            if (a!=b){
+
+            // Convert characters to lowercase for comparison
+            char a = tolower(s[i]);
+            char b = tolower(s[j]);
+
+            // If the characters don't match, return false
+            if (a != b) {
                 return false;
             }
+
+            // Move pointers inward
             i++;
             j--;
         }
