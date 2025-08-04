3290a56929063ab443b9a519b55f889ccd9ecc7a
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-04 13:59:48 +0530

    Time: 34 ms (76.25%) | Memory: 92.3 MB (44.78%) - LeetSync

---
+++ 2689-rearranging-fruits/rearranging-fruits.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    long long minCost(vector<int>& b1, vector<int>& b2) {
+        
+        using ll = long long;
+        int n = b1.size();
+        ll mine = INT_MAX;
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < n; i++){
+            mine = min(mine, (ll)min(b1[i], b2[i]));
+            mpp[b1[i]]++;
+            mpp[b2[i]]--;
+        }
+
+        vector<ll> transfer; int num; int freq; int rep;
+
+        for (auto i : mpp){
+            freq = i.second;
+            num = i.first;
+            if (freq%2!=0) return -1;
+            rep = abs(freq/2);
+            while(rep){
+                transfer.push_back((ll)num);
+                rep--;
+            }
+        }
+
+        sort(transfer.begin(), transfer.end());
+
+        ll cost = 0;
+        for (int i = 0; i < transfer.size()/2; i++){
+            cost += min(mine*2, transfer[i]);
+        }
+
+        return cost;
+    }
+};
