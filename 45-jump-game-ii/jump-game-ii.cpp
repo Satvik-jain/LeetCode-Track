2630fb817ca189c2d7cbdfd1b9cd950a67a975be
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-05 05:44:12 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (98.01%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int helper(int index, vector<int>& nums, vector<int>& dp){
-        int n = nums.size();
-        if (index >= n-1){
-            return 0;
-        }
-        if (dp[index] != -1) return dp[index];
-        int mine = 1e9;
-        for (int i = 1; i<=nums[index]; i++){
-            mine = min(mine, 1+helper(index+i, nums, dp));
-        }
-        return dp[index] = mine;
-    }
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return helper(0, nums, dp);
+        int n = nums.size();
+        int maxInd = 0;
+        int jump = 0;
+        int end = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            maxInd = max(maxInd, i + nums[i]);
+            if (i == end) {
+                jump++;
+                end = maxInd;
+            }
+        }
+
+        return jump;
     }
 };
