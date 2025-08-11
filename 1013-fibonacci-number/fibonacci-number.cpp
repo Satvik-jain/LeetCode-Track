9927331b12a2f3b79468e3037c19bd61968ed124
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-11 18:38:57 +0530

    Time: 2 ms (77.50%) | Memory: 7.9 MB (36.75%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        // bottom up approach
-        if(n==0) return 0;
-        int prev = 1;
-        int prev2 = 0;
-        for (int i = 2; i < n+1; i++){
-            int curr = prev  + prev2;
-            prev2 = prev;
-            prev = curr;
+        int i = 0; int j = 1;
+        int temp = 0;
+        for (int k = 0; k < n; k++){
+            temp = i;
+            i = j;
+            j = temp + j;
         }
-        return prev;
+        return (n)? i:0;
     }
 };
