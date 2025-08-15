3ad65907c2e5ff830e17748e6509a30de3835f66
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-15 22:06:22 +0530

    Time: 788 ms (13.30%) | Memory: 388.4 MB (12.79%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
-    bool isPalindrome(const string &s) {
-        int l = 0, r = s.size() - 1;
-        while (l < r) {
-            if (s[l] != s[r]) return false;
-            l++;
-            r--;
+    int helper(string s, int left, int right, int ans){
+        if (left < 0 || right >= s.length() || s[left] != s[right]) return ans;
+        if (s[left]==s[right]){
+            ans++;
+            left--;
+            right++;
         }
-        return true;
+        return helper(s, left, right, ans);
     }
-
-    void helper(int &ans, string &nums, string &temp, int index) {
-        if (index == nums.size()) return;
-
-        // Extend substring one char at a time
-        temp.push_back(nums[index]);
-        if (isPalindrome(temp)) ans++;
-        
-        helper(ans, nums, temp, index + 1); // keep extending
-        temp.pop_back();                    // backtrack
-    }
-
-    void startFromEachIndex(int &ans, string &nums, int start) {
-        string temp = "";
-        helper(ans, nums, temp, start);      // build substrings from this start
-        if (start + 1 < nums.size()) 
-            startFromEachIndex(ans, nums, start + 1); // move to next start
-    }
-
-    int countSubstrings(string nums) {
+    int countSubstrings(string s) {
         int ans = 0;
-        startFromEachIndex(ans, nums, 0);
+        for (int i = 0; i < s.length(); i++){
+            ans+=helper(s, i, i, 0) + helper(s, i, i+1, 0);
+        }
         return ans;
     }
 };
