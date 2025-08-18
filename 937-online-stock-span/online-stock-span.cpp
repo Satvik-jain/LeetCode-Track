4b8927b0c860ee10748fdcde9285b991aae5c183
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-18 23:18:32 +0530

    Time: 29 ms (66.19%) | Memory: 92.1 MB (27.71%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,21 +1,19 @@
 class StockSpanner {
 public:
-    stack<pair<int,int>> st;  // {price, span}
+    stack<pair<int,int>> st;  
 
     StockSpanner() {
-        // stack starts empty, nothing else required
+
     }
     
     int next(int price) {
         int span = 1;
-        
-        // while stack top price <= current price, accumulate spans
         while(!st.empty() && st.top().first <= price) {
             span += st.top().second;
             st.pop();
         }
         
-        st.push({price, span}); // store current price and its span
-        return span;            // return only today's span
+        st.push({price, span}); 
+        return span;           
     }
 };
