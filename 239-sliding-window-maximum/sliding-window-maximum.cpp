89c1c80d2ed0cf0bc61b864ec0ec76e0a71408a7
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-19 00:11:08 +0530

    Time: 55 ms (22.30%) | Memory: 153.1 MB (20.31%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -2,26 +2,35 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
-        int i = 0, j = 0;
-        // int max = INT_MIN;
-        unordered_map<int, int> mpp;
-        priority_queue<int> q;
         vector<int> ans;
-        while(j < n){
-            mpp[nums[j]]++;
-            if (j - i + 1 < k){
-                q.push(nums[j]);
+        int i = 0, j = 0;
+
+        // max-heap {value, index}
+        priority_queue<pair<int,int>> pq;
+
+        while (j < n) {
+            // add current element
+            pq.push({nums[j], j});
+
+            // if window size < k, just expand
+            if (j - i + 1 < k) {
                 j++;
+                continue;
             }
-            else if (j - i + 1 == k){
-                q.push(nums[j]);
-                
-                while (!q.empty() && mpp[q.top()] == 0) q.pop();
-                ans.push_back(q.top());
-                mpp[nums[i]]--;
-                i++; j++;
+
+            // remove stale elements (whose index < i)
+            while (!pq.empty() && pq.top().second < i) {
+                pq.pop();
             }
+
+            // top is max of current window
+            ans.push_back(pq.top().first);
+
+            // slide the window forward
+            i++;
+            j++;
         }
+
         return ans;
     }
 };
