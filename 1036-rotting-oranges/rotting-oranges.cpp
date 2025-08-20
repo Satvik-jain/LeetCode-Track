ddf23118e7d71045797644c469ffce8523b36b8c
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-21 03:44:49 +0530

    Time: 3 ms (28.64%) | Memory: 17.2 MB (19.03%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,53 +1,47 @@
 class Solution {
-private:
-    int bfs(vector<vector<int>>& vis, vector<vector<int>>& grid){
-        queue <pair<pair<int, int>, int>> q;
-        int m = grid.size();
-        int n = grid[0].size();
-        int time = 0;
-        int count = 0;
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                if (grid[i][j] == 2){
-                    q.push({{i,j},time});
-                    vis[i][j]++;
-                }
-                if (grid[i][j] == 1)count++;
-            }
-        }
-        int cnt = 0;
-        while(!q.empty()){
-            int a = q.front().first.first;
-            int b = q.front().first.second;
-            time = q.front().second;
-            q.pop();
-            int rows[] = {-1,0,1,0};
-            int cols[] = {0,1,0,-1};
-            for (int i = 0; i < 4 ; i++){
-                int newr = a + rows[i];
-                int newc = b + cols[i];
-                // if (abs(i) + abs(j) == 2) continue;
-                if (newr >= 0 && newc >= 0 && newr < grid.size() && 
-                    newc < grid[0].size()){
-                    if (grid[newr][newc] == 1 && vis[newr][newc] == 0){
-                        vis[newr][newc] = 1;
-                        grid[newr][newc] = 2;
-                        q.push({{newr, newc},time + 1});
-                        cnt++;
-                    }
-                
-                }
-            }
-        }
-        if(count!=cnt) return -1;
-        else return time;
-    }
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> vis(m, vector<int> (n, 0));
-        int time = bfs(vis, grid);
-        return time;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<pair<int, int>, int>> q;
+
+        int time = 0;
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (grid[i][j] == 2) q.push({{i, j}, time});
+            }
+        }
+
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            time = q.front().second;
+            ans = max(ans, q.front().second);
+            grid[r][c] = 2;
+            q.pop();
+            int ra[] = {-1, 0, 1, 0};
+            int ca[] = {0, -1, 0, 1};
+            for(int i = 0; i < 4; i++){
+                int nr = r+ra[i];
+                int nc = c+ca[i];
+                if (nc<0||nr<0||nc>=m||nr>=n) continue;
+                if (grid[nr][nc] == 1){
+                    grid[nr][nc] = 2;
+                    q.push({{nr, nc}, time+1});
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (grid[i][j] == 1) return -1;
+            }
+        }
+
+        return ans;
     }
 };
