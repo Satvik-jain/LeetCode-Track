61dfa83b8e3bc86c2880e527c81ddf288f1a6cf9
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-20 22:53:49 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (94.58%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -1,15 +1,18 @@
 class Solution {
   public:
-    void helper(TreeNode* root, vector<string>& ans, string v){
+    void helper(TreeNode* root, vector<string>& ans, string &v){
         if (!root) return;
+        int len = v.length();
         v+=to_string(root->val);
         if (!root->left && !root->right){
             ans.push_back(v);
+            v.resize(len);
             return;
         }
         v+="->";
         helper(root->left, ans, v);
         helper(root->right, ans, v);
+        v.resize(len);
     }
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string> ans;
