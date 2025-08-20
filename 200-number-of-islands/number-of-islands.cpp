2dd99d1147719c701a63776161ae0fb35607ff1a
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-21 03:33:26 +0530

    Time: 29 ms (44.65%) | Memory: 19.5 MB (41.35%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
-    void bfs(int i, int j, int n, int m, vector<vector<char>>& grid){
-        queue<pair<int,int>> q;
-        q.push({i,j});
-        while(!q.empty()){
-            int r = q.front().first;
-            int c = q.front().second;
-            grid[r][c] = '0';
-            q.pop();
-            for(int i: {-1, 0, 1}){
-                for(int j : {-1, 0, 1}){
-                    int nr = r + i;
-                    int nc = c + j;
-                    if (nr<0 || nr > n-1 || nc<0 || nc > m-1) continue;
-                    if(abs(i)+abs(j) == 2){
-                        continue;
-                    }
-                    else{
-                        if(grid[nr][nc] == '1'){
-                            grid[nr][nc] = '0';
-                            q.push({nr,nc});
-                        }
-                    }
-                }
-            }
-        }
-    }
     int numIslands(vector<vector<char>>& grid) {
-        int ans = 0;
         int n = grid.size();
         int m = grid[0].size();
-        // vector<vector<int>> vis(n, vector<int> (m,0));
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j]=='0') continue;
-                else{
-                    bfs(i, j, n, m, grid);
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<int, int>> q;
+        int ans = 0;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (grid[i][j] == '1' && vis[i][j] == 0){
                     ans++;
+                    q.push({i, j});
+                    vis[i][j] = 1;
+                    while(!q.empty()){
+                        int r = q.front().first;
+                        int c = q.front().second;
+                        vis[r][c] = 1;
+                        q.pop();
+                        int ra[] = {-1, 0, 1, 0};
+                        int ca[] = {0, -1, 0, 1};
+                        for(int i = 0; i < 4; i++){
+                            int nr = r+ra[i];
+                            int nc = c+ca[i];
+                            if (nc<0||nr<0||nc>=m||nr>=n) continue;
+                            if (grid[nr][nc] == '1' && vis[nr][nc] == 0){
+                                q.push({nr, nc});
+                                vis[nr][nc] = 1;
+                            }
+                        }
+                    }
                 }
             }
         }
