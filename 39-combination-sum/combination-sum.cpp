acaaf5ed848cafda3a8e9fe35cd2c68ce282b93d
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-24 05:28:52 +0530

    Time: 3 ms (55.67%) | Memory: 17.8 MB (29.22%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void helper(vector<int>& candidates, int target, int idx, vector<vector<int>> & ans, vector<int>& v){
+        if (target == 0){
+            ans.push_back(v);
+            return;
+        }
+        if (idx >= candidates.size() || target < 0) return;
+        helper(candidates, target, idx+1, ans, v);
+        v.push_back(candidates[idx]);
+        helper(candidates, target - candidates[idx], idx, ans, v);
+        v.pop_back();
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> v;
+        helper(candidates, target, 0, ans, v);
+        return ans;
+    }
+};
