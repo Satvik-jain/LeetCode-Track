3c02183f72e0afc031a1e541c5f6a25d226942b3
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-24 05:31:54 +0530

    Time: 3 ms (55.67%) | Memory: 17.2 MB (30.77%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -6,6 +6,7 @@ public:
             return;
         }
         if (idx >= candidates.size() || target < 0) return;
+        if (candidates[idx] > target) return;
         helper(candidates, target, idx+1, ans, v);
         v.push_back(candidates[idx]);
         helper(candidates, target - candidates[idx], idx, ans, v);
@@ -14,6 +15,7 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> v;
+        sort(candidates.begin(), candidates.end());
         helper(candidates, target, 0, ans, v);
         return ans;
     }
