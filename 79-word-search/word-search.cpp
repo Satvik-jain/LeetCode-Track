c9087fe70e80cc62204b4d0ea84c1033d960e6f8
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-25 19:00:56 +0530

    Time: 427 ms (46.08%) | Memory: 11.7 MB (34.37%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>> & vis, int i, int j, vector<vector<char>>& board, string word, int index){
+    bool dfs(int i, int j, vector<vector<char>>& board, string word, int index){
         if (index == word.length()) return true;
-        vis[i][j] = 1;
+        int temp = board[i][j];
+        board[i][j] = '#';
         for (int a: {-1, 0, 1}){
             for (int b: {-1, 0, 1}){
                 int r = i+a;
                 int c = j+b;
                 if (abs(a)+abs(b)==2) continue;
                 if (r<0 || c<0 || r>=board.size() || c>=board[0].size()) continue;
-                if (vis[r][c] == 0){
+                if (board[r][c] != '#'){
                     if (board[r][c] == word[index]){
-                        vis[r][c] = 1;
-                        if (dfs(vis, r, c, board, word, index+1)) return true;
-                        vis[r][c] = 0;
+                        if (dfs(r, c, board, word, index+1)) return true;
                     }
                 }
             }
         }
+        board[i][j] = temp;
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
@@ -30,12 +30,14 @@ public:
                 if (word[0] == board[i][j]) first.push_back({i, j});
             }
         }
+
         for(auto a: first){
-            vector<vector<int>> vis(n, vector<int>(m, 0));
+            // vector<vector<int>> vis(n, vector<int>(m, 0));
             int i = a.first;
             int j = a.second;
-            if (dfs(vis, i, j, board, word, 1)) return true;
+            if (dfs(i, j, board, word, 1)) return true;
         }
+
         return false;
     }
 };
