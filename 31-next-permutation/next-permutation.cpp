7061586fa0395cad4e7150cef9004c01777c38f5
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-27 16:55:00 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (33.18%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        // next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+        int pivot = -1;
+        for (int i = n-1; i >= 1; i--){
+            if (nums[i-1] < nums[i]){
+                pivot = i-1;
+                break;
+            }
+        }
+        if (pivot == -1){
+            sort(nums.begin(), nums.end());
+            return;
+        }
+        for (int i = n-1; i >= 0; i--){
+            if (nums[pivot] < nums[i]){
+                swap(nums[pivot], nums[i]);
+                for(auto i : nums){
+                    cout << i;
+                }
+                break;
+            }
+        }
+        sort(nums.begin()+pivot+1, nums.end());
     }
 };
