42fd0ae81726a6e7c60cbc09329267d304e68b89
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-27 19:07:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (9.44%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> copy(n, vector<int>(n, 0));
+        auto copy = matrix;
         for (int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 copy[j][n-1-i] = matrix[i][j];
