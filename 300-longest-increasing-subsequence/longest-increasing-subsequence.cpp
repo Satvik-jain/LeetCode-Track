4279777f5277871e018c2cb9003751960347a526
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-28 11:29:40 +0530

    Time: 580 ms (11.60%) | Memory: 296.5 MB (13.70%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,18 +1,22 @@
 class Solution {
-  public:
-    int lengthOfLIS(vector<int>& arr) {
-        // Code here
-        int n = arr.size();
-        vector<int> dp(n, 1);
-        int maxe = 1;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < i; j++){
-                if (arr[i] > arr[j] && 1+dp[j] > dp[i]){
-                    dp[i] = dp[j] + 1;
-                }
-            }
-            maxe = max(maxe, dp[i]);
+public:
+    int helper(vector<int> & nums, int indx, int last, vector<vector<int>> &dp){
+        if (indx == nums.size()){
+            return 0;
         }
-        return maxe;
+        
+        if (dp[indx][last+1] != -1) return dp[indx][last+1];
+
+        int temp = helper(nums, indx+1, last, dp);
+
+        if (last == -1 || nums[last] < nums[indx]){
+            return dp[indx][last+1]=max(1+helper(nums, indx+1, indx, dp), temp);
+        }
+
+        return dp[indx][last+1]=temp;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        vector<vector<int>> dp(nums.size(), vector<int>(nums.size()+1, -1));
+        return helper(nums, 0, -1, dp);
     }
 };
