09ee02433e3eff4d03cf75cc84715e8ec2f4396e
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-29 14:02:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (88.20%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int index, int target, vector<vector<int>>& dp){
+    int helper(vector<int>& nums, int target, vector<int>& dp){
         if (target == 0){
             return 1;
         }
-        if (index == nums.size() || target < 0) return 0;
-        if (dp[index][target] != -1) return dp[index][target];
+        if (target < 0) return 0;
+        if (dp[target] != -1) return dp[target];
         
         int ways=0;
         for (int i = 0; i < nums.size(); i++){
-            ways+=helper(nums, i, target-nums[i], dp);
+            ways+=helper(nums, target-nums[i], dp);
         }
 
-        return dp[index][target]=ways;
+        return dp[target]=ways;
     }
     int combinationSum4(vector<int>& nums, int target) {
 
-        vector<vector<int>> dp(nums.size(), vector<int>(target+1, -1));
-        return helper(nums, 0, target, dp);
+        vector<int> dp(target+1, -1);
+        return helper(nums, target, dp);
     }
 };
