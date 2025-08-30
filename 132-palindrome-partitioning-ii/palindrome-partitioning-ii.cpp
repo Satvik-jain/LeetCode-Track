f5ec2c8057252a063bf6105c760c95d3557892bc
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-30 06:07:14 +0530

    Time: 1754 ms (19.73%) | Memory: 9.4 MB (73.58%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -7,26 +7,22 @@ bool isPalindrome(string &s, int i, int j) {
         }
         return true;
     }
-    int PP(string &s, int i, int j, vector<vector<int>>& dp, int ans){
-        // cout << ans << endl;
-        if (i >= j) return 0;
-        
-        if (dp[i][j] != -1) return dp[i][j];
-        if (isPalindrome(s, i, j)) return 0; // whole substring already palindrome
-        
-        int cut = 0;
-        for (int k = i; k < j; k++){
-            if (isPalindrome(s, i, k)){
-                cut = 1+PP(s,k+1,j, dp, ans);
-                ans = min(ans, cut);
+    int PP(string &s, int i, vector<int>& dp){
+        if (i == s.length()) return 0;
+        int n = s.length();
+        int mine = INT_MAX;
+        if (dp[i]!=-1) return dp[i];
+        for(int j = i; j < n; j++){
+            if (isPalindrome(s, i, j)){
+                int ways = 1 + PP(s, j+1, dp);
+                mine = min(mine, ways);
             }
         }
-        return dp[i][j]=ans;
+        return dp[i] = mine;
     }
     int minCut(string &s) {
         int n = s.length();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        int ans = INT_MAX;
-        return PP(s, 0, s.length()-1, dp, ans);
+        vector<int> dp(n, -1);
+        return PP(s, 0, dp)-1;
     }
 };
