d26e7a0a5aec4fc9c97ec4a7070495529f0738ce
Satvik Jain <satvik.jain.kht@gmail.com>
2025-08-30 18:26:19 +0530

    Time: 20 ms (70.91%) | Memory: 13.9 MB (78.29%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int helper(int i, int j, vector<int> &cuts, vector<vector<int>> &dp){
-        if (i > j) return 0;
-        int mine = INT_MAX;
-        if (dp[i][j]!=-1) return dp[i][j];
-        for (int ind = i; ind <= j; ind++){
-            int cost = cuts[j+1] - cuts[i-1] + helper(i, ind-1, cuts, dp) + helper(ind+1, j, cuts, dp);
-            mine = min(mine, cost);
-        }
-        return dp[i][j] = (mine==INT_MAX)?0:mine;
-    }
     int minCost(int n, vector<int>& cuts) {
-        cuts.push_back(0); cuts.push_back(n);
         int c = cuts.size();
+        cuts.push_back(0); cuts.push_back(n);
+        
         sort(cuts.begin(), cuts.end());
-        vector<vector<int>> dp(c+1, vector<int>(c+1, -1));
-        return helper(1, c-2, cuts, dp);
+        vector<vector<int>> dp(c+2, vector<int>(c+2, 0));
+        
+        for (int i = c; i >= 1; i--){
+            for (int j = 1; j<=c; j++){
+                if (i > j) continue;
+                int mine = INT_MAX;
+                for (int ind = i; ind <= j; ind++){
+                    int cost = cuts[j+1] - cuts[i-1] + dp[i][ind-1] + dp[ind+1][j];
+                    mine = min(mine, cost);
+                }
+                if (mine == INT_MAX) mine = 0; 
+                dp[i][j] = mine;
+            }
+        } 
+        
+        return dp[1][c];
     }
 };
