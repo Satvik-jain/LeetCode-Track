b78d37949608bb0cb66eb02ffc3ec8b6f5909b5b
Satvik Jain <satvik.jain.kht@gmail.com>
2025-09-06 23:50:45 +0530

    Time: 4 ms (91.49%) | Memory: 52.8 MB (75.93%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector <int> adj[n];
-        for(int i = 0; i < graph.size(); i++){
-            for (int j = 0; j < graph[i].size(); j++){
-                adj[graph[i][j]].push_back(i);
+    bool dfs(vector<vector<int>> &adj, int i, vector<int>& vis, vector<int> &ans, vector<int>& cycle){
+        vis[i] = 1;
+        for (int j : adj[i]){
+            if (!vis[j]) {if(dfs(adj, j, vis, ans, cycle)){
+                cycle[i] = 1;
+                return true;
+            }}
+            else if(vis[j]==1){
+                cycle[i] = 1;
+                return true;
             }
         }
-        queue<int>q;
-        vector <int> indegree(n,0);
-        for (int i = 0; i<n; i++){
-            indegree[i] = graph[i].size();
-        }
-        for (int i = 0; i < n; i++){
-            if (indegree[i] == 0) q.push(i);
-        }
-        vector <int> ans;
-        while(!q.empty()){
-            int top = q.front();
-            q.pop();
-            ans.push_back(top);
-            for (int i: adj[top]){
-                indegree[i]--;
-                if (!indegree[i]){
-                    q.push(i);
-                }
+        vis[i] = 2;
+        if(cycle[i] == 0)ans.push_back(i);
+        return false;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
+        int n = adj.size();
+        vector<int> vis(n, 0), cycle(n, 0);
+        vector<int> ans;
+        for(int i = 0; i < n; i++){
+            if (!vis[i]){
+                dfs(adj, i, vis, ans, cycle);
             }
-
         }
         sort(ans.begin(), ans.end());
         return ans;
