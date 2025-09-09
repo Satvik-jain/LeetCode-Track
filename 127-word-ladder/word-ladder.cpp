ea47c05c7a8e5c4722cf8a184bbe15c78f9e9b1d
Satvik Jain <satvik.jain.kht@gmail.com>
2025-09-09 15:16:22 +0530

    Time: 84 ms (43.71%) | Memory: 21 MB (83.59%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -14,7 +14,9 @@ public:
             for (int i =0; i < beginWord.size(); i++){
                 for(int j = 0; j < 26; j++){
                     char c = 'a' + j;
-                    string newWord = word; newWord[i] = c;
+                    string newWord = word;
+                    if (newWord[i]!=c) newWord[i] = c;
+                    else continue;
                     if (newWord == endWord){
                         return dis+1;
                     }
