e1c94b2af3fca65c1412f1210420738c1e76773c
Satvik Jain <satvik.jain.kht@gmail.com>
2025-09-10 23:54:02 +0530

    Time: 55 ms (38.66%) | Memory: 90.8 MB (46.68%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mpp;
+        for (int i : nums){
+            mpp[i]++;
+            if (mpp[i] == 2) return true;
+        }
+        return false;
+    }
+    
+};
