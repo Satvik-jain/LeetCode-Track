0bda2644d9a755ea6ff9b7b6696f871fc8a2fcd7
Satvik Jain <satvik.jain.kht@gmail.com>
2025-09-11 08:28:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (79.26%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,17 +1,17 @@
-class Solution{
+class Solution {
 public:
-    bool helper(string & s, int i, int j){
-        if (i > j) return true;
-        if (!isalnum(s[i])){
-            return helper(s, i+1, j);
-        }
-        if (!isalnum(s[j])){
-            return helper(s, i, j-1);
-        }
-        if (tolower(s[i]) == tolower(s[j])) return helper(s, i+1, j-1);
-        else return false;
-    }
     bool isPalindrome(string s) {
-        return helper(s, 0, s.length()-1);
+        int i = 0; int j = s.length()-1;
+        while(i < j){
+            if (!isalnum(s[i])){
+                i++; continue;
+            }
+            if (!isalnum(s[j])){
+                j--; continue;
+            }
+            if (tolower(s[i])!=tolower(s[j])) return false;
+            i++; j--;
+        }
+        return true;
     }
 };
