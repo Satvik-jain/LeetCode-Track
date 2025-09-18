99ad60923c78a6fbd1fabe85bd6a177e3faaaf6e
Satvik Jain <satvik.jain.kht@gmail.com>
2025-09-18 23:14:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (32.82%) - LeetSync

--- 457-circular-array-loop/circular-array-loop.cpp
+++ 457-circular-array-loop/circular-array-loop.cpp
@@ -6,7 +6,6 @@ public:
         for (int i = 0; i < n; i++){
             if (!vis[i]){
                 int dir = nums[i]/abs(nums[i]);
-                cout << dir << endl;
                 path_vis = zeros;
                 queue<int> q;
                 q.push(i);
