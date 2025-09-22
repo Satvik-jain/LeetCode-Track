4c09f80df3fa29f1932c289cd967b01d0c7242b1
Satvik Jain <satvik.jain.kht@gmail.com>
2025-09-23 04:47:20 +0530

    Time: 405 ms (82.10%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select w2.id
+from weather as w1
+join weather as w2
+on w1.recordDate = DATE_SUB(w2.recordDate, INTERVAL 1 DAY)
+where w1.temperature < w2.temperature;
