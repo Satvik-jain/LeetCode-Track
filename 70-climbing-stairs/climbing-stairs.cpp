9840a2ae8d748a9133be436bd47ff463beae084c
Satvik Jain <satvik.jain.kht@gmail.com>
2025-09-29 16:40:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (18.49%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -9,11 +9,14 @@ public:
     int climbStairs(int n) {
         vector<int> dp(n+1, 0);
         if (n == 1) return 1;
-        if ( n==2 ) return 2;
-        dp[1] = 1, dp[2] = 2;
+        if (n == 2) return 2;
+        int a = 1, b = 2;
+        int c;
         for (int i = 3; i < n+1; i++){
-            dp[i] = dp[i-1] + dp[i-2]; 
+            c = a + b;
+            a = b;
+            b = c;
         }
-        return dp[n];
+        return c;
     }
 };
