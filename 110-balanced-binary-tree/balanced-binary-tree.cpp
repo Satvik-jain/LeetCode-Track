8cf7e4fa0361b8ce52654d98e815f6661c552483
Satvik Jain <satvik.jain.kht@gmail.com>
2025-09-30 16:47:09 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (56.55%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -1,20 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int helper(TreeNode* root) {
+    int helper(TreeNode* root, bool &ans){
         if (!root) return 0;
-
-        int left = helper(root->left);
-        if (left == -1) return -1;   // left subtree is unbalanced
-
-        int right = helper(root->right);
-        if (right == -1) return -1;  // right subtree is unbalanced
-
-        if (abs(left - right) > 1) return -1; // current node not balanced
-
-        return 1 + max(left, right); // return height if balanced
+        int left = 1+helper(root->left, ans);
+        int right = 1+helper(root->right, ans);
+        ans = ans && (abs(left-right)<=1);
+        return max(left, right);
     }
-
     bool isBalanced(TreeNode* root) {
-        return helper(root) != -1;
+        bool ans = true;
+        helper(root, ans);
+        return ans;
     }
 };
