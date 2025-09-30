1b490213c3ecd18bde66f364e1767061203ed458
Satvik Jain <satvik.jain.kht@gmail.com>
2025-09-30 16:54:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (77.84%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -11,28 +11,15 @@
  */
 class Solution {
 public:
-    // void helper(TreeNode* p, vector<TreeNode*>& st){
-    //     if (!p){
-    //         st.push_back(nullptr);
-    //         return;
-    //     }
-    //     st.push_back(p);
-    //     helper(p->left, st);
-    //     helper(p->right, st);
-    // }
+    bool helper(TreeNode* p, TreeNode* q){
+        if (!p && !q) return true;
+        if ((!p && q) || (p && !q)) return false;
+        if (p->val == q->val){
+            return (helper(p->right, q->right) && helper(p->left, q->left));
+        }
+        else return false;
+    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p && !q) return true;
-        if ((!p && q)||(p && !q)) return false;
-        if (p->val != q->val) return false;
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-        // vector<TreeNode*> st1, st2;
-        // helper(p, st1); helper(q, st2);
-        // if (st1.size() != st2.size()) return false;
-        // for (int i = 0; i < st1.size(); i++){
-        //     if(!st1[i] && !st2[i]) continue;
-        //     if ((!st1[i] && st2[i])||(st1[i] && !st2[i])) return false;
-        //     if (st1[i]->val != st2[i]->val) return false;
-        // }
-        // return true;
+        return helper(p, q);
     }
 };
