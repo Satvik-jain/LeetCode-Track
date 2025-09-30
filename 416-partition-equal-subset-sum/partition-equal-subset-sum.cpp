933d5daca413954da92253b97e0413d1970762bb
Satvik Jain <satvik.jain.kht@gmail.com>
2025-09-30 11:59:44 +0530

    Time: 167 ms (25.76%) | Memory: 111.3 MB (19.14%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,32 +1,24 @@
 class Solution {
-  public:
-    // bool f(vector<int>& arr, int n, int W, vector<vector<int>> &dp){
-    //     if (dp[n][W] != -1) return dp[n][W];
-    //     if (W == 0) return 1;
-    //     if (n == 0) return 0;
-    //     if (W >= arr[n-1]) return dp[n][W] = (f(arr, n-1, W, dp) || f(arr, n-1, W-arr[n-1], dp));
-    //     else return dp[n][W] = f(arr, n-1, W, dp);
-    // }
-    bool canPartition(vector<int>& arr) {
-        // code here
-        int n = arr.size();
-        int sum = 0;
-        for (int i = 0; i < n; i++){
-            sum+=arr[i];
-        }
+public:
+    bool helper(vector<int>& nums, int n, int sum, vector<vector<int>> &dp, int i){
+        if (sum <= 0) return (sum==0);
+        if (i >= n) return false;
+
+        if (dp[i][sum]!=-1) return dp[i][sum];
+
+        bool take = false, not_take = false;
+
+        if (nums[i] <= sum) take = helper(nums, n, sum-nums[i], dp, i+1);
+
+        not_take = helper(nums, n, sum, dp, i+1);
+
+        return dp[i][sum] = (take || not_take);
+    }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = accumulate(nums.begin(), nums.end(), 0);
         if (sum%2!=0) return false;
-        int W = sum/2;
-        vector<vector<int>> dp(n+1, vector<int>(W+1, 0));
-        for (int i = 0; i < n+1; i++){
-            dp[i][0] = 0;
-        }
-        dp[0][0] = 1;
-        for(int i = 1; i < n+1; i++){
-            for (int j = 1; j < W+1; j++){
-                if (j >= arr[i-1]) dp[i][j] = (dp[i-1][j] || dp[i-1][j-arr[i-1]]);   
-                else dp[i][j] = dp[i-1][j];
-            }
-        }
-        return dp[n][W];
+        vector<vector<int>> dp(n+1, vector<int>((sum/2)+1, -1));
+        return helper(nums, n, sum/2, dp, 0);
     }
 };
