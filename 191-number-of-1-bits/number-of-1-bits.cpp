82603adf54851d408b2d747e854709e4581ebe4e
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-02 15:03:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (98.63%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,8 +3,8 @@ public:
     int hammingWeight(int n) {
         int ans = 0;
         while(n){
-            if (n%2!=0) ans++;
-            n/=2;
+            if (n%2) ans++;
+            n = n>>1;
         }
         return ans;
     }
