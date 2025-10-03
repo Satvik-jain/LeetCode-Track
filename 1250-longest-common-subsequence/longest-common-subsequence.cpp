b7ea1ed494387840b7e6c9eb91e234c87dde21b9
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-04 03:23:05 +0530

    Time: 47 ms (11.39%) | Memory: 27.6 MB (15.68%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,31 +1,18 @@
 class Solution {
-  public:
-    // int f(string &s1, string& s2, int n, int m, vector<vector<int>> & dp){
-    //     if (n == 0 || m == 0) return 0;
-    //     if (dp[n][m] != -1) return dp[n][m];
-    //     else if (s1[n-1] == s2[m-1]) return dp[n][m] = 1 + f(s1,s2,n-1, m-1, dp);
-    //     else return dp[n][m] =  max(f(s1,s2,n-1,m, dp), f(s1, s2, n, m-1, dp));
-    // }
-    int longestCommonSubsequence(string &s1, string &s2) {
-        // code here
-        int n = s1.size();
-        int m = s2.size();
-        vector<vector<int>> dp(s1.size()+1, vector<int>(s2.size()+1, 0));
-        int maxe = INT_MIN;
-        int ans = 0;
-        for (int i = 1; i < n+1; i++){
-            for (int j = 1; j < m+1; j++){
-                if (s1[i-1] == s2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                    ans++;
-                    maxe = max(ans, maxe);
-                }
-                else{
-                    dp[i][j] =  max(dp[i][j-1], dp[i-1][j]);
-                    ans = 0;
-                }
-            }
+public:
+    int helper(string &a, string &b, int n, int m, vector<vector<int>>& t){
+        if (m==0 || n==0) return 0;
+        if (t[n][m] != -1) return t[n][m];
+        if (a[n-1] == b[m-1]){
+            return t[n][m] = 1 + helper(a, b, n-1, m-1, t);
         }
-        return dp[n][m];
+        return t[n][m] = max(helper(a, b, n, m-1, t), helper(a, b, n-1, m, t));
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        if (m==0 || n==0) return 0;
+        vector<vector<int>> t(n+1, vector<int>(m+1, -1));
+        return helper(text1, text2, n, m, t);
     }
 };
