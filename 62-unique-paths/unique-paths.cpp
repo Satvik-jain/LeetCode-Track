ba1328f11e9d19299dd8fe202739a9cc002727c9
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-04 03:10:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (86.69%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
+    int helper(int n, int r){
+        if (r == 1) return n;
+        if (r == 0) return 1;
+        if (r < 0) return 0; 
+        return (int)(((long double)(n - r + 1) / r) * helper(n, r - 1) + 0.5L);
+
+    }
     int uniquePaths(int m, int n) {
-        long long res = 1;
-        int r = min(m-1, n-1);
-        int total = m + n - 2;
-
-        // Compute C(total, r) = total! / (r! * (total-r)!)
-        for (int i = 1; i <= r; i++) {
-            res = res * (total - r + i) / i;
-        }
-
-        return (int)res;
+        int p = max(m,n);
+        int q = min(m,n);
+        int r = q-1;
+        n = p+q-2;
+        return helper(n, r);
     }
 };
