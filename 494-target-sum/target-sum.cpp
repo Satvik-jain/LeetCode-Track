b4a920fff5d94973e893a4bc8f30824a6613aead
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-05 05:18:55 +0530

    Time: 866 ms (11.51%) | Memory: 11.5 MB (91.61%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,22 +1,14 @@
 class Solution {
-  public:
-    int findTargetSumWays(vector<int>& arr, int d) {
-        // Code here
-        int n = arr.size();
-        int sum = accumulate(arr.begin(), arr.end(), 0);
-        int W = (sum+d)/2;
-        if (d > sum || -1*sum > d) return 0;
-        if ((sum+d)%2 != 0) return 0;
-        vector<vector<int>> dp(n+1, vector<int> (W+1, 0));
-        for (int i = 0; i < n+1; i++){
-            dp[i][0] = 1;
-        }
-        for (int i = 1; i < n+1; i++){
-            for (int j = 0; j < W+1; j++){
-                if (j >= arr[i-1]) dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]];
-                else dp[i][j] = dp[i-1][j];
-            }
-        }
-        return dp[n][W];
+public:
+    int helper(vector<int>& nums, int target, int i, int n){
+        if (i == n && target == 0) return 1;
+        if (i == n) return 0;
+        int tot = 0;
+        tot += helper(nums, target-nums[i], i+1, n) +
+                helper(nums, target+nums[i], i+1, n);
+        return tot; 
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return helper(nums, target, 0, nums.size());
     }
 };
