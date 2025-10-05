97c43385a45110dbec4a072517b144ce3ccef7c1
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-06 03:34:39 +0530

    Time: 2 ms (96.33%) | Memory: 22.8 MB (74.26%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    bool is_valid(int mid, vector<int>& piles, int h){
-        int pre = 0;
-        for (int i : piles){
-            pre = pre + i/mid;
-            if (i%mid!=0) pre+=1;
+    bool is_valid(int mid, vector<int>& piles, int h) {
+        long long pre = 0;  // use long long to avoid overflow
+        for (int i : piles) {
+            pre += (i + mid - 1) / mid;  // ceil division
             if (pre > h) return false;
         }
         return true;
     }
+
     int minEatingSpeed(vector<int>& piles, int h) {
         int i = 1, j = *max_element(piles.begin(), piles.end());
-        int res = 0;
-        while(i <= j){
-            int mid = (i+j)/2;
-            if (is_valid(mid, piles, h)){
+        int res = j;  // worst case: eat at max pile size
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (is_valid(mid, piles, h)) {
                 res = mid;
-                j = mid-1;
+                j = mid - 1;
+            } else {
+                i = mid + 1;
             }
-            else i = mid+1;
         }
         return res;
     }
 };
+
