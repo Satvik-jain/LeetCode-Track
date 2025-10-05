3e118b0fbbdf5e093715f17b38123777f202b932
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-05 17:08:33 +0530

    Time: 11 ms (21.87%) | Memory: 20.9 MB (13.59%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>> &adj, vector<int>& topo, vector<int>& vis, int i, int & cycle){
-        vis[i] = 2;
-        for (int j : adj[i]){
-            if (vis[j] == 2){
-                return false;
-            }
-            if(!vis[j]){
-                if (!dfs(adj, topo, vis, j, cycle)) return false;
-            }
-        }
-        vis[i] = 1;
-        topo.push_back(i);
-        return true;
-    }
     bool canFinish(int n, vector<vector<int>>& pre) {
         vector<vector<int>> adj(n);
         for(auto i : pre){
             adj[i[0]].push_back(i[1]);
         }
         vector<int> topo;
-        vector<int> vis(n, 0);
-        int cycle = 0;
-        for (int i = 0; i < n; i++){
-            if(!vis[i]){
-                if (!dfs(adj, topo, vis, i, cycle)) return false;
+        vector<int> indegree(n, 0);
+        for (vector<int> i : adj){
+            for (int j : i){
+                indegree[j]++;
             }
         }
-        return true;
+        stack<int> st;
+        for(int i = 0; i < n; i++){
+            if (indegree[i] == 0){
+                st.push(i);
+                topo.push_back(i);
+            }
+        }
+        while(!st.empty()){
+            int i = st.top(); st.pop();
+            for(int j : adj[i]){
+                indegree[j]--;
+                if (indegree[j] == 0){
+                    topo.push_back(j);
+                    st.push(j);
+                }
+            }
+        }
+        return (topo.size() == n);
     }
 };
