381da6161093a37c3c632a9a36ea44adc0173a2d
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-05 15:59:11 +0530

    Time: 9 ms (29.26%) | Memory: 20.3 MB (22.84%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &adj, vector<int>& topo, vector<int>& vis, int i, int & cycle){
+    bool dfs(vector<vector<int>> &adj, vector<int>& topo, vector<int>& vis, int i, int & cycle){
         vis[i] = 2;
         for (int j : adj[i]){
-            if(!vis[j]){
-                dfs(adj, topo, vis, j, cycle);
-            }
             if (vis[j] == 2){
-                cycle = 1;
-                break;
+                return false;
+            }
+            if(!vis[j]){
+                if (!dfs(adj, topo, vis, j, cycle)) return false;
             }
         }
         vis[i] = 1;
         topo.push_back(i);
+        return true;
     }
     bool canFinish(int n, vector<vector<int>>& pre) {
         vector<vector<int>> adj(n);
@@ -24,10 +24,9 @@ public:
         int cycle = 0;
         for (int i = 0; i < n; i++){
             if(!vis[i]){
-                dfs(adj, topo, vis, i, cycle);
+                if (!dfs(adj, topo, vis, i, cycle)) return false;
             }
-            if (cycle) return false;
         }
-        return !cycle;
+        return true;
     }
 };
