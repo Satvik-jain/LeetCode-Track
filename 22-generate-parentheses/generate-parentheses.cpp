3e1e311651a3765e730a4e21d9a4abb641020bb5
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-10 11:30:24 +0530

    Time: 5 ms (19.02%) | Memory: 17.1 MB (10.10%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
-
-    void helper(vector<string>& v, string op, int o, int c){
-        // cout << o << " " << c << endl;
-        if ((o == c) && (c!=0)){
-            op.push_back('(');
-            helper(v, op, o-1, c);
-            return;
+    void helper(int n, vector<string>& ans, string s, int open, int close){
+        if (open == n && close == n){
+            ans.push_back(s); return;
         }
-        if(c==0){
-            v.push_back(op); 
-            return;
-        }
-        string op1 = op;
-        string op2 = op;
-        if(o!=0){
-            op1.push_back('(');
-            helper(v, op1, o-1, c);
-        }
-        op2.push_back(')');
-        helper(v, op2, o, c-1);  
+        string s_open = s+"(";
+        string s_close = s+")";
+        if (open > close) helper(n, ans, s_close, open, close+1);
+        if (open < n) helper(n, ans, s_open, open+1, close);
     }
-
     vector<string> generateParenthesis(int n) {
-        int o = n, c = n;
-        string op = "";
-        vector <string> v;
-        helper(v, op, o, c);
-        return v;
+        vector<string> ans;
+        helper(n, ans, "(", 1, 0);
+        return ans;
     }
 };
