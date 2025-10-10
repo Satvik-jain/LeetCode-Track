ab7e72fa6f25e998eba8f26f3910d6641fdcfc0f
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-10 11:00:17 +0530

    Time: 47 ms (12.79%) | Memory: 86.2 MB (23.13%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,40 +1,27 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int ans = INT_MIN;
-
         int n = heights.size();
-
-        vector<int> nse(n);
-        vector<int> pse(n);
-
-        stack<int> st;
-        for(int i = 0; i < n; i++){
-            while(!st.empty() && heights[st.top()] >= heights[i]) st.pop();
-            if(st.empty()){
-                pse[i] = -1;
-            } else {
-                pse[i] = st.top();
-            }
-            st.push(i);
+        vector<int> nse(n), pse(n);
+        stack<pair<int, int>> st;
+        for (int i = n-1; i >= 0; i--){
+            while(st.size() && st.top().first >= heights[i]) st.pop();
+            if (st.size()) nse[i] = st.top().second;
+            else nse[i] = n;
+            st.push({heights[i], i});
         }
-
-        while(!st.empty()) st.pop();
-        for(int i = n-1; i >= 0; i--){
-            while(!st.empty() && heights[st.top()] >= heights[i]) st.pop();
-            if(st.empty()){
-                nse[i] = n;
-            } else {
-                nse[i] = st.top();
-            }
-            st.push(i);
+        while(st.size()) st.pop();
+        for (int i = 0; i < n; i++){
+            while(st.size() && st.top().first >= heights[i]) st.pop();
+            if (st.size()) pse[i] = st.top().second;
+            else pse[i] = -1;
+            st.push({heights[i], i});
         }
-
-        for(int  i = 0; i < n; i++){
-            cout << (nse[i]-pse[i]-1)*heights[i] << endl;
-            ans = max(ans, (nse[i]-pse[i]-1)*heights[i]);
+        int maxe = INT_MIN;
+        for (int i = 0; i < n; i++){
+            maxe = max(maxe, (nse[i] - pse[i] - 1)*heights[i]);
+            // cout << nse[i] << " " << pse[i] << " " << maxe << endl;
         }
-
-        return ans;
+        return maxe;
     }
 };
