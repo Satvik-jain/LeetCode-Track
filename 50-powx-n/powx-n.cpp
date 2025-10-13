64a42982db00eabe1a38eff1ec92c8db1e0f37b1
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-14 02:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (71.25%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    double helper(double x, long long n){
-        if (n==1) return x;
-        if (n==0) return 1;
-        if (n%2 == 0){
-            double k = helper(x, n/2);
-            return k*k;
+double ow(double x, int n) {
+        // cout << n;
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if (n%2==0){
+            double temp = ow(x, n/2);
+            return temp*temp;
         }
         else{
-            double k = helper(x, n/2);
-            return k * k * x;
+            double temp = ow(x, n/2);
+            return x*temp*temp;
         }
+        return 1;
     }
     double myPow(double x, int n) {
-        long long N = n;
-        double ans = helper(x, abs(N));
-        return (n >= 0) ? ans : 1/ans;
+        double ans = ow(x, n);
+        if (n < 0){
+            ans = 1/ans;
+            cout << ans;
+        }
+        return ans;
     }
 };
