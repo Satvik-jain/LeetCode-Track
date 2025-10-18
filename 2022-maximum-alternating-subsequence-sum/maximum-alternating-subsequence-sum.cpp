9eb8fcd84f715436deb1b934dcfd1d98e2edfcc2
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-18 16:09:15 +0530

    Time: 304 ms (11.92%) | Memory: 168 MB (13.87%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
-    long long helper(vector<int>& nums, int i, int sign, vector<vector<long long>>& dp){
-        if (i >= nums.size()) return 0;
-        if (dp[i][sign+1]!=-1) return dp[i][sign+1];
-        long long taken = sign*nums[i]+helper(nums, i+1, sign*-1, dp);
-        long long not_taken = helper(nums, i+1, sign, dp);
-        return dp[i][sign+1] = max(taken, not_taken);
+    long long helper(vector<int>& nums, int i, int sign, vector<vector<long long>>& dp) {
+        if (i == nums.size()) return 0;
+
+        int idx = (sign == 1 ? 0 : 1);
+        if (dp[i][idx] != -1) return dp[i][idx];
+
+        long long take = sign * nums[i] + helper(nums, i + 1, -sign, dp);
+        long long skip = helper(nums, i + 1, sign, dp);
+
+        return dp[i][idx] = max(take, skip);
     }
+
     long long maxAlternatingSum(vector<int>& nums) {
-        vector<vector<long long>> dp(nums.size(), vector<long long>(3, -1));
+        vector<vector<long long>> dp(nums.size(), vector<long long>(2, -1));
         return helper(nums, 0, 1, dp);
     }
 };
