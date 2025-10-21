32c771aaba3f76e04038a742639b9260efe6676e
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-21 07:26:32 +0530

    Time: 1208 ms (59.59%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,14 +1,16 @@
-# Write your MySQL query statement below
-select p.product_name, s.year, s.price 
-from sales as s
-left join product as p
-on p.product_id = s.product_id;
 
 
 
 
+/*
+This query retrieves each product’s name, sales year, and price.
+It works with both JOIN and LEFT JOIN because:
+- Every product_id in the sales table exists in the product table.
+- The relationship is enforced through a foreign key.
+Hence, both joins produce the same output.
+*/
 
-
-
-
-# 
+SELECT p.product_name, s.year, s.price
+FROM sales AS s
+LEFT JOIN product AS p
+ON p.product_id = s.product_id;
