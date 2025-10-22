fd52f263446932290a0045287d2abe4c43667733
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-23 03:58:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (20.63%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if (word.length() == 1) return true;
+        if (isupper(word[0])){
+            bool first = isupper(word[1]);
+            for (int i = 2; i < word.length(); i++){
+                if (first != (bool)isupper(word[i])) return false;
+            }
+        }
+        else{
+            for (int i = 1; i < word.length(); i++){
+                if (isupper(word[i])) return false;
+            }
+        }
+        return true;
+    }
+};
