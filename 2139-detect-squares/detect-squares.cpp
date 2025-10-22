4fb19aa90be363ffd7c7a0bc544153e41c60aef9
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-23 01:14:17 +0530

    Time: 301 ms (12.04%) | Memory: 118.6 MB (26.38%) - LeetSync

--- 2139-detect-squares/detect-squares.cpp
+++ 2139-detect-squares/detect-squares.cpp
@@ -15,7 +15,7 @@ public:
         for (auto& i: mpp){
             vector<int> pt = i.first;
             int x1 = pt[0], y1 = pt[1];
-            if (x1 == x || y1 == y || abs(x1-x)!=abs(y1-y)) continue;
+            if ( abs(x1-x)!=abs(y1-y)  || x1 == x || y1 == y) continue;
             vector<int> p1 = {x1, y};
             vector<int> p2 = {x, y1};
             if (mpp[p1] && mpp[p2]){
