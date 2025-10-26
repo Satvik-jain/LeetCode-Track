59f2d42da05e286b8ac5433d342d3732e729476e
Satvik Jain <satvik.jain.kht@gmail.com>
2025-10-26 16:08:09 +0530

    Time: 55 ms (19.39%) | Memory: 49.7 MB (24.30%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-    // int helper(vector<int>& coins, int amount, int n, int i, vector<vector<int>> &dp){
-    //     if (amount == 0) return 0;
-    //     if (amount < 0) return 1e9;
+    int helper(vector<int>& coins, int amount, vector<vector<int>> &dp, int n, int index){
+        if (index >= n) return 1e9;
+        if (amount < 0) return 1e9;
+        if (amount == 0) return 0;
+        if (dp[index][amount]!=-1) return dp[index][amount];
 
-    //     if (dp[i][amount]!=-1) return dp[i][amount];
+        int taken = 1e9, not_taken = 1e9;
 
-    //     int take = 1e9, not_take = take;
+        if (coins[index]<=amount) taken = 1+helper(coins, amount-coins[index], dp, n, index);
+        not_taken = helper(coins, amount, dp, n, index+1);
 
-    //     if (coins[i] <= amount){
-    //         take = 1 + helper(coins, amount-coins[i], n, i, dp);
-    //     }
-
-    //     if (i+1 < n) not_take = helper(coins, amount, n, i+1, dp);
-
-    //     return dp[i][amount] = min(take, not_take);
-    // }
+        return dp[index][amount] = min(taken, not_taken);
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, 1e9));
-        for (int i = 0; i < n; i++) dp[i][0] = 0;
-        for (int i = 0; i < n; i++){
-            for (int j = 1; j <= amount; j++){
-                int take = 1e9, not_take = take;
-                if (coins[i] <= j){
-                    take = 1 + dp[i][j-coins[i]];
-                }
-                if (i > 0) not_take = dp[i-1][j];
-
-                dp[i][j] = min(take, not_take);
-            }
-        }
-        return (dp[n-1][amount]==1e9) ? -1: dp[n-1][amount];
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        int ans = helper(coins, amount, dp, n, 0);
+        return (ans==1e9)?-1:ans;
     }
 };
