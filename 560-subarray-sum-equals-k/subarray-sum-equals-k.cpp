b23a92dfab666c4637e9bfc0faf8c967a1962786
Satvik Jain <satvik.jain.kht@gmail.com>
2025-11-08 12:23:27 +0530

    Time: 43 ms (63.47%) | Memory: 45.3 MB (68.35%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> mpp;
-        mpp[0] = 1;
-        int count = 0;
+        int i = 0;
+        int n = nums.size();
         int sum = 0;
-        for (int n : nums){
-            sum+=n;
-            if(mpp.find(sum-k) != mpp.end()){
-                count += mpp[sum-k];
-            }
-            mpp[sum]++;
-        }
-        return count;
+        int cnt = 0;
+        unordered_map<int, int> mpp;
+mpp[0] = 1;
+for (; i < n; i++) {
+    sum += nums[i];
+    if (mpp.count(sum - k)) cnt += mpp[sum - k];
+    mpp[sum]++;
+}
+
+        return cnt;
     }
 };
-auto init = atexit([](){ofstream("display_runtime.txt")<<"0";});
