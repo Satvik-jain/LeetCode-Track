7801ad9df1d6f0ac7cbdbce9991b7307b485287e
Satvik Jain <satvik.jain.kht@gmail.com>
2025-11-08 12:23:44 +0530

    Time: 38 ms (79.04%) | Memory: 45.3 MB (75.58%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -6,13 +6,12 @@ public:
         int sum = 0;
         int cnt = 0;
         unordered_map<int, int> mpp;
-mpp[0] = 1;
-for (; i < n; i++) {
-    sum += nums[i];
-    if (mpp.count(sum - k)) cnt += mpp[sum - k];
-    mpp[sum]++;
-}
-
+        mpp[0] = 1;
+        for (; i < n; i++) {
+            sum += nums[i];
+            if (mpp.count(sum - k)) cnt += mpp[sum - k];
+            mpp[sum]++;
+        }
         return cnt;
     }
 };
