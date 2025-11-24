487e4e05d875c5a0aa9524ebde6ba9f7700c0cb0
Satvik Jain <satvik.jain.kht@gmail.com>
2025-11-24 17:42:31 +0530

    Time: 602 ms (22.75%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,7 +1,7 @@
+# Write your MySQL query statement below
 select customer_id
 from customer
 group by customer_id
 having count(distinct product_key) = (
-    select count(product_key)
-    from product
+    select count(*) from product
 )
